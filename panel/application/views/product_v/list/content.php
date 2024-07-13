1b5de2893f6ce703d69cfe24e3ad9b29fbdf91bc
oguzhannokur <oguzhannokur33@gmail.com>
2024-07-13 21:24:54 +0300

    urunler modulunun urun listesi icin html sablonu olusturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,35 @@
-urun listesi content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <table class="table table-hover table-striped">
+                <thead>
+                <th>#id</th>
+                <th>url</th>
+                <th>Başlık</th>
+                <th>Açıklama</th>
+                <th>Durumu</th>
+                <th>İşlem</th>
+
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>monitör Askisi</td>
+                        <td>360 derece kullanabilen monitor askisidir</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                        </td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>

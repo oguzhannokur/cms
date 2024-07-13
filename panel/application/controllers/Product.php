e5fcb67e1de9cd7d38664867260d1a7a4ea1d789
oguzhannokur <oguzhannokur33@gmail.com>
2024-07-13 20:59:01 +0300

    viewFolder ve subViewFolder yapisi getirildi. View klasorleri list,add,update olarak 3e bolundu. Urunler listelemesin view yapisi icin giris olusturuldu

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Product extends CI_Controller {
 
-	public $viewFolder = "";
+    public $viewFolder = "";
 
     public function __construct()
     {
@@ -11,16 +11,16 @@ class Dashboard extends CI_Controller {
         error_reporting(0);
         $this->load->library("session");
         $this->load->helper('url');
-        $this->viewFolder="dashboard_v";
+        $this->viewFolder="product_v";
     }
 
     public function index()
-	{
+    {
         $viewData = new stdClass();
-        $viewData->viewFolder= $this->viewFolder;
-        $viewData->subViewFolder= "list";
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
 
-		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-	}
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
 
 }

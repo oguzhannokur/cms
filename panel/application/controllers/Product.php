0525f6329f80b40e47b4c83926c382705d3343b8
oguzhannokur <oguzhannokur33@gmail.com>
2024-07-16 04:13:49 +0300

    urun update islemi gerceklestirildi

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -113,4 +113,63 @@ class Product extends CI_Controller {
 
     }
 
+    public function update($id){
+        $this->load->library("form_validation");
+
+        //Kurallar yazılır
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required" => "<b>{field}</b> alanı doldurulmalıdır"
+            )
+        );
+
+        //Form Validation Çalıştırılır
+        //TRUE - FALSE döner
+        $validate=$this->form_validation->run();
+
+        if($validate){
+
+            $update = $this->product_model->update(
+                array(
+                    "id"    => $id
+                ),
+                array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSEO($this->input->post("title")),
+                )
+            );
+
+            if($update){
+
+                redirect(base_url("product"));
+
+            } else{
+
+                redirect(base_url("product"));
+
+            }
+
+        } else{
+            $viewData = new stdClass();
+
+            /** Tablodan verilerin getirilmesi */
+            $item = $this->product_model->get(
+                array(
+                    "id"    => $id
+                )
+            );
+
+            /** view'e gönderilecek değişkenlerin set edilmesi */
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "update";
+            $viewData->form_error=true;
+            $viewData->item=$item;
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+    }
+
 }
